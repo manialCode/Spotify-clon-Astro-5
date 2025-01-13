2318643c241350e232c5e2410455413d3fe9aafd
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-13 12:40:35 -0300

    udpt: app

---
+++ src/components/react/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const Pause = () => (
+  <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16">
+    <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+  </svg>
+);
+
+const Play = () => (
+  <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16">
+    <path d="M3 1.713a.7.7 0 0 1 1.105-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.105 14.894a.7.7 0 0 1-1.105-.607V1.713z"></path>
+  </svg>
+);
+
+export function Player() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    return (
+        <div className="flex flex-row justify-between items-center w-full px-4 z-50">
+            <div className="">
+                CurrentSong
+            </div>
+            <div className="grid place-content-center gap-4 flex-1">
+                <div className="flex justify-center">
+                    <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
+                        {isPlaying ? <Pause />:<Play />  }
+                    </button>
+                </div>
+            </div>
+            <div className="">
+                Volume 
+            </div>
+        </div>
+    )
+}
---
+++ src/components/svelte/Greetings.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greetings = "";
+
+  if (currentHour < 12) greetings = "Buenos dias";
+  else if (currentHour < 18) greetings = "Buenas tardes";
+  else greetings = "Buenas noches";
+</script>
+
+<h1 class="font-bold text-3xl">{greetings}</h1>
