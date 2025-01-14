0a0ce444c170bd527b7ebe7cef6c8a020e158cf9
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-14 14:18:42 -0300

    updt:Player functions working

--- src/components/react/Player.jsx
+++ src/components/react/Player.jsx
@@ -1,34 +1,67 @@
-import { useState } from "react";
+import { usePlayerStore } from "@/store/playerStore"; // estado global
+import { useEffect, useRef, useState } from "react";
 
-const Pause = () => (
+export const Pause = () => (
   <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16">
     <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
   </svg>
 );
 
-const Play = () => (
+export const Play = () => (
   <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16">
     <path d="M3 1.713a.7.7 0 0 1 1.105-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.105 14.894a.7.7 0 0 1-1.105-.607V1.713z"></path>
   </svg>
 );
 
+const CurrentSong = ({ image, title }) => {
+  return (
+    <div className={`flex items-center gap-5 relative overflow-hidden`}>
+      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+        <img src={image} alt={title} />
+      </picture>
+
+      <h3 className="font-bold block">{title}</h3>
+    </div>
+  );
+};
+
 export function Player() {
-    const [isPlaying, setIsPlaying] = useState(false);
-    return (
-        <div className="flex flex-row justify-between items-center w-full px-4 z-50">
-            <div className="">
-                CurrentSong
-            </div>
-            <div className="grid place-content-center gap-4 flex-1">
-                <div className="flex justify-center">
-                    <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
-                        {isPlaying ? <Pause />:<Play />  }
-                    </button>
-                </div>
-            </div>
-            <div className="">
-                Volume 
-            </div>
+  const { isPlaying, setIsPlaying, currentMusic } = usePlayerStore(
+    (state) => state
+  );
+  const audioRef = useRef();
+
+  useEffect(() => {
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const { playlist, songs, song } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.play();
+    }
+  }, [currentMusic]);
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+  return (
+    <div className="flex flex-row justify-between items-center w-full px-4 z-50">
+      <div className="">
+        <CurrentSong {...currentMusic.song} />
+      </div>
+      <div className="grid place-content-center gap-4 flex-1">
+        <div className="flex justify-center">
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
+            {isPlaying ? <Pause /> : <Play />}
+          </button>
         </div>
-    )
+      </div>
+
+      <div className="grid place-content-center"></div>
+
+      <audio ref={audioRef} />
+    </div>
+  );
 }
