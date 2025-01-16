db94d879193d294da21d4a108ba75d6eb6074110
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-16 19:54:59 -0300

    updt:app functionality

--- src/components/SideMenu/PlaylistItemCard.tsx
+++ src/components/SideMenu/PlaylistItemCard.tsx
@@ -1,8 +1,8 @@
-import { MusicsTablePlayButton } from "@/components/MusicsTable/MusicTablePlayButton";
 import { TitleCard } from "@/components/utils/TitleCard";
 import { usePlayerStore } from "@/store/playerStore";
 import type { Playlist } from "@/lib/data";
 import { VolumeFull } from "@/icons/VolumeIcons";
+import { SidePlayButton } from "@/components/SideMenu/SidePlayButton";
 
 interface Props {
   playlist: Playlist;
@@ -21,36 +21,36 @@ export const PlaylistItemCard = ({ playlist }: Props) => {
   const isCurrent = currentSong?.albumId === albumId;
 
   return (
-    <a
-      href={`/playlist/${id}`}
-      className="group playlist-item flex relative p-2 overflow-hidden items-center gap-2 rounded-md hover:bg-zinc-800"
-    >
+    <article className="relative group">
       {/* Botón para reproducir música */}
       {currentSong && (
         <div className="absolute top-[1.5rem] left-6 opacity-0 transition-all group-hover:opacity-100 z-10">
-          <MusicsTablePlayButton song={currentSong} isCurrentSong={isCurrent} />
+          <SidePlayButton id={id} />
         </div>
       )}
-      <picture className="h-12 w-12 flex-none after-img">
-        <img
-          src={cover}
-          alt={`Cover of ${title} by ${artistsString}`}
-          className="object-cover w-full h-full rounded-md"
-        />
-      </picture>
-
-      <div className="flex flex-auto flex-col truncate">
-        {/* Componente TitleCard con client:load */}
-        <TitleCard albumId={albumId} title={title} />
-
-        <span className="text-xs text-gray-400">{artistsString}</span>
-      </div>
-      {isCurrent && (
-        <span className="text-green-500">
-          {" "}
-          <VolumeFull />{" "}
-        </span>
-      )}
-    </a>
+      <a
+        href={`/playlist/${id}`}
+        className="playlist-item flex  p-2 overflow-hidden items-center gap-2 rounded-md hover:bg-zinc-800"
+      >
+        <picture className="h-12 w-12 flex-none after-img">
+          <img
+            src={cover}
+            alt={`Cover of ${title} by ${artistsString}`}
+            className="object-cover w-full h-full rounded-md"
+          />
+        </picture>
+        <div className="flex flex-auto flex-col truncate">
+          {/* Componente TitleCard con client:load */}
+          <TitleCard albumId={albumId} title={title} />
+          <span className="text-xs text-gray-400">{artistsString}</span>
+        </div>
+        {isCurrent && (
+          <span className="text-green-500">
+            {" "}
+            <VolumeFull />{" "}
+          </span>
+        )}
+      </a>
+    </article>
   );
 };
