a493fe2492b9373dd9a0d1f85f67ea8281211d6c
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-18 20:30:35 -0300

    updt:MobileCard styles

--- src/components/mobile/MobileCard.tsx
+++ src/components/mobile/MobileCard.tsx
@@ -1,37 +1,44 @@
-import { type Song } from "@/lib/data";
 import { usePlayerStore } from "@/store/playerStore";
 
-import { MusicsTablePlayButton } from "../MusicsTable/MusicTablePlayButton";
+import { MobilePlayButton } from "./MobilePlayButton";
 
 const MobileCard = () => {
-  const { song, playlist } = usePlayerStore((state) => state.currentMusic);
+  const { currentMusic, setCurrentMusic, isPlaying, setIsPlaying } =
+    usePlayerStore((state) => state);
+  const { playlist, song } = currentMusic;
   const bg = playlist?.color.dark;
 
   return (
-    <a
-      href={`/playlist/${playlist?.albumId}/song/${song?.id}`}
-      className={`flex items-center gap-5 overflow-hidden justify-between rounded-lg h-[4.2rem]`}
+    <div
       style={{ backgroundColor: bg ? bg : "#27272a" }}
+      className="flex items-center gap-5 relative overflow-hidden justify-between rounded-lg h-[4.2rem]"
     >
-      <div className="flex items-center gap-5">
-        <picture className="w-16 h-16 p-1 aspect-square overflow-hidden">
-          <img
-            src={song?.image}
-            className="rounded"
-            alt={`Cover of ${song?.title}`}
-          />
-        </picture>
-        <div className="flex  flex-col text-left">
-          <h3 className="font-semibold text-sm block">{song?.title}</h3>
-          <span className="opacity-80 text-xs">
-            {song?.artists ? song?.artists?.join(" • ") : "No album or artist?"}
-          </span>
+      <a
+        href={`/playlist/${playlist?.albumId}/song/${song?.id}`}
+        className="w-full"
+      >
+        <div className="flex items-center gap-5">
+          <picture className="w-16 h-16 p-1 aspect-square overflow-hidden">
+            <img
+              src={song?.image}
+              className="rounded"
+              alt={`Cover of ${song?.title}`}
+            />
+          </picture>
+          <div className="flex  flex-col text-left">
+            <h3 className="font-semibold text-sm block">{song?.title}</h3>
+            <span className="opacity-80 text-xs">
+              {song?.artists
+                ? song?.artists?.join(" • ")
+                : "No album or artist?"}
+            </span>
+          </div>
         </div>
+      </a>
+      <div className="mr-5 z-10 absolute right-2 bottom-[1.15rem]">
+        {song && <MobilePlayButton />}
       </div>
-      <div className="mr-5">
-        {song && <MusicsTablePlayButton song={song} />}
-      </div>
-    </a>
+    </div>
   );
 };
 
