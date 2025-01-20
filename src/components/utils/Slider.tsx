4bbfdfe02fd8838ee2442d07a2c9857426821d5b
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-20 17:00:37 -0300

    feat: corregir tipos de referencia en el componente Slider para mejorar la tipificación y la legibilidad

--- src/components/utils/Slider.tsx
+++ src/components/utils/Slider.tsx
@@ -1,9 +1,9 @@
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
-import cn from "clsx"
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import cn from "clsx";
 
 export const Slider = React.forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
@@ -20,8 +20,6 @@ export const Slider = React.forwardRef<
 
     <SliderPrimitive.Thumb className="hidden group-hover:block h-3 w-3 bg-white rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
-))
+));
 
-Slider.displayName = SliderPrimitive.Root.displayName
-
-
+Slider.displayName = SliderPrimitive.Root.displayName;
