29f821a9ed203d5c8ad3d491969e974f9ab65719
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-22 13:44:25 -0300

    feat: agregar componentes SongC y WhiteSlider, mejorar controles de reproducción y diseño responsivo

--- src/components/Player/PlayerControl.tsx
+++ src/components/Player/PlayerControl.tsx
@@ -2,7 +2,11 @@ import { Next, Play, Prev, Pause } from "@/icons/PlayerIcons";
 import { usePlayerStore } from "@/store/playerStore";
 import { useCurrentMusic } from "../hooks/UseCurrentMusic";
 
-export function PlayerControl() {
+interface Props {
+  xl: boolean;
+}
+
+export function PlayerControl({ xl }: Props) {
   const { currentMusic, isPlaying, setCurrentMusic, setIsPlaying } =
     usePlayerStore((state) => state);
   const { getNextSong, getPreviousSong } = useCurrentMusic(currentMusic);
@@ -28,25 +32,32 @@ export function PlayerControl() {
     }
   };
 
+  const classPlay = xl ? "w-10 h-auto p-2" : "w-4 h-auto";
+  const classNextPrev = xl ? "w-11 h-auto p-2" : "w-4 h-auto";
+
   return (
     <div className="flex gap-5 items-center justify-center">
       <button
         className=" hover:opacity-85 hover:scale-110"
         onClick={onPrevSong}
       >
-        <Prev />
+        <Prev className={classNextPrev} />
       </button>
       <button
         className="bg-white text-black rounded-full p-2 hover:scale-110 hover:opacity-85"
         onClick={onPlayPause}
       >
-        {isPlaying ? <Pause className="sm" /> : <Play className="sm" />}
+        {isPlaying ? (
+          <Pause className={classPlay} />
+        ) : (
+          <Play className={classPlay} />
+        )}
       </button>
       <button
         className=" hover:opacity-85 hover:scale-110"
         onClick={onNextSong}
       >
-        <Next />
+        <Next className={classNextPrev} />
       </button>
     </div>
   );
