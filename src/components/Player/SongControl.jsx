f1037dd12253de4d924f57da6229b23e4ea03246
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-22 13:44:35 -0300

    feat: agregar componente White al control de canción para mejorar la experiencia en dispositivos móviles

--- src/components/Player/SongControl.jsx
+++ src/components/Player/SongControl.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Slider } from "../utils/Slider";
+import { Slider, White } from "../utils/Slider";
 
 export const SongControl = ({ audio }) => {
   const [currentTime, setCurrentTime] = useState(0);
@@ -46,21 +46,41 @@ export const SongControl = ({ audio }) => {
   };
 
   return (
-    <div className="flex gap-x-3 pt-2">
-      <span className="opacity-60 w-12 text-right text-sm font-light">
-        {formatTime(currentTime)}
-      </span>
-      <Slider
-        defaultValue={[0]}
-        max={duration}
-        min={0}
-        className="w-96"
-        value={[currentTime]}
-        onValueChange={handleSliderChange}
-      />
-      <span className="opacity-60 w-12 text-sm font-light">
-        {formatTime(duration)}
-      </span>
+    <div>
+      <div className="player:flex gap-x-3 pt-2 hidden">
+        <span className="opacity-60 w-12 text-right text-sm font-light">
+          {formatTime(currentTime)}
+        </span>
+        <Slider
+          defaultValue={[0]}
+          max={duration}
+          min={0}
+          className="w-96"
+          value={[currentTime]}
+          onValueChange={handleSliderChange}
+        />
+        <span className="opacity-60 w-12 text-sm font-light">
+          {formatTime(duration)}
+        </span>
+      </div>
+      <div className=" player:hidden gap-3 pt-2 flex flex-col items-center mx-1 w-[65vw]">
+        <White
+          defaultValue={[0]}
+          max={duration}
+          min={0}
+          value={[currentTime]}
+          className="w-full "
+          onValueChange={handleSliderChange}
+        />
+        <div className="flex justify-between w-full">
+          <span className="opacity-60 w-12 text-left text-sm font-light">
+            {formatTime(currentTime)}
+          </span>
+          <span className="opacity-60 w-12 text-sm font-light text-right">
+            {formatTime(duration)}
+          </span>
+        </div>{" "}
+      </div>
     </div>
   );
 };
